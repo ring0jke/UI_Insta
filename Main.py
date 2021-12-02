3cf1d9ca492ce29e60bc59787b3dc09884097f03
kappa <enjoyhosting@gmail.com>
2021-12-02 15:59:33 +0300

    new

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import json_prep
-
-
-
+import accGenerate
+import email_handle
+import pyautogui
 '''
 1)Запускаем прокси
 2)Получаем email от email.py
@@ -14,7 +14,12 @@ import json_prep
 
 '''
 
+username = accGenerate.username()
+name = accGenerate.generatingName()
+#email = email_handle.getFakeMail()
+password = accGenerate.generatePassword()
+print(username,password)
 
 
-json_prep.CreateAccount(username='kappuchinaxo282',name='Kappa Mappa',email='new_test@910interior.ru',password='2Kappa823!')
+#json_prep.CreateAccount(username='kappuchinaxo282',name='Kappa Mappa',email='new_test@910interior.ru',password='2Kappa823!')
 #json_prep.SetEmailCode('483992')
